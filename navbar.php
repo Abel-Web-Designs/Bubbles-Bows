fa11a0f9aeb5c00d3ca0757acc8bc2040fff1b3f
Abel Gaming <lcpdpolice4939@gmail.com>
2023-12-10 21:33:34 -0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><img src="img/logo_navbar.png"></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="pricing.php">Pricing</a></li>
+        <li><a href="#about.php">About Us</a></li>
+        <li><a href="#contact.php">Contact</a></li>
+    </ul>
+</nav>
